e8bc83765e3788331d17d4be632f342b34bc5aab
Gregory Linscheid <linscheid.greg@gmail.com>
2024-06-28 22:30:30 -0700

    :wrench: mdx blog is going

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE = 'Greg Linscheid';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export function formatTitle(pageTitle: string) {
+  return `${SITE_TITLE} | ${pageTitle}`;
+}
